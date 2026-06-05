07b84d7f617bdf815c32891b76a69e61f5833fa0
pallavipatil05 <pallavi25official@gmail.com>
2026-06-05 09:23:49 +0530

    Added analytics API for resume statistics

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const authRoutes = require('./routes/authRoutes')
 const resumeRoutes = require('./routes/resumeRoutes')
 const historyRoutes = require('./routes/historyRoutes')
 const app = express()
+const analyticsRoutes = require('./routes/analyticsRoutes')
 
 app.use(cors())
 app.use(express.json())
@@ -13,7 +14,7 @@ app.use(express.json())
 app.use('/api/auth', authRoutes)
 app.use('/api/resume', resumeRoutes)
 app.use('/api/history', historyRoutes)
-
+app.use( '/api/analytics',analyticsRoutes)
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
